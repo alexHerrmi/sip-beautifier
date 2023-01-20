a2f08be3df341fa947186716531b9cc540040edd
alexHerrmi <alexander.herrmann@parloa.com>
2023-01-20 22:00:42 +0100

    init

--- src/popup.js
+++ src/popup.js
@@ -1,112 +1,60 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('beautifyButton').addEventListener('click', () => {
+    const orderedObject = {};
 
-import './popup.css';
-
-(function () {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: (cb) => {
-      chrome.storage.sync.get(['count'], (result) => {
-        cb(result.count);
+    const iframe = document.getElementById('sandbox');
+    window.addEventListener('message', (event) => {
+      console.log(event.data);
+      let arrOfLines = event.data.split('\r\n').filter((e) => e.length > 0);
+      arrOfLines.map((e) => {
+        let key = e.substring(0, e.indexOf(':'));
+        orderedObject[key] = e.substring(e.indexOf(':') + 1, e.length);
       });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
+      console.log(arrOfLines);
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
+      const existingChild = document.getElementsByClassName('text-box')[0];
+      if (existingChild)
+        document.getElementById('render').removeChild(existingChild);
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
+      const contentDiv = document.getElementById('render');
+      const wholeTextDiv = document.createElement('div');
+      wholeTextDiv.classList.add('text-box');
+      contentDiv.style.display = 'block';
+
+      Object.keys(orderedObject).forEach((e) => {
+        const newFieldDiv = document.createElement('div');
+        newFieldDiv.classList.add('newFieldDiv');
+        const keySpan = document.createElement('span');
+        keySpan.classList.add('keySpan');
+
+        const valueSpan = document.createElement('span');
+        valueSpan.classList.add('valueSpan');
+
+        keySpan.innerText = e + ':';
+        valueSpan.innerText = orderedObject[e];
+
+        newFieldDiv.appendChild(keySpan);
+        newFieldDiv.appendChild(valueSpan);
+        wholeTextDiv.appendChild(newFieldDiv);
       });
+
+      contentDiv.appendChild(wholeTextDiv);
     });
 
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
+    const sipInvite = document.getElementById('sipInput').value;
+    iframe.contentWindow.postMessage("'" + sipInvite + "'", '*');
+  });
+
+  document.getElementById('copyToClipboard').addEventListener('click', () => {
+    document.getElementById('copiedSpan').classList.add('clipboardFade');
+    setTimeout(() => {
+      document.getElementById('copiedSpan').classList.remove('clipboardFade');
+    }, 1000);
+
+    let textToCopy = '';
+    document.querySelectorAll('.newFieldDiv').forEach((e) => {
+      textToCopy += e.textContent + '\n';
     });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get((count) => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            (response) => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get((count) => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreCounter);
-
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    (response) => {
-      console.log(response.message);
-    }
-  );
-})();
+    let result = navigator.clipboard.writeText(textToCopy);
+  });
+});
