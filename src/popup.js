845c78d4b04f05f55ab7c07a49dbc0cf1a6f4c1a
Alexander <alexander.herrmann@fruchtec.de>
2023-01-24 23:18:12 +0100

    Added some style elements

--- src/popup.js
+++ src/popup.js
@@ -11,38 +11,40 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(event.data);
         let arrOfLines = event.data.split('\r\n').filter((e) => e.length > 0);
         arrOfLines.map((e) => {
-          let key = e.substring(0, e.indexOf(':'));
+          let key = e.substring(0, e.indexOf(':'))
+            ? e.substring(0, e.indexOf(':'))
+            : '<no-key>';
           orderedObject[key] = e.substring(e.indexOf(':') + 1, e.length);
         });
-        console.log(arrOfLines);
+        if (Object.keys(orderedObject).length > 1) {
+          const existingChild = document.getElementsByClassName('text-box')[0];
+          if (existingChild)
+            document.getElementById('render').removeChild(existingChild);
 
-        const existingChild = document.getElementsByClassName('text-box')[0];
-        if (existingChild)
-          document.getElementById('render').removeChild(existingChild);
+          const contentDiv = document.getElementById('render');
+          const wholeTextDiv = document.createElement('div');
+          wholeTextDiv.classList.add('text-box');
+          contentDiv.style.display = 'block';
 
-        const contentDiv = document.getElementById('render');
-        const wholeTextDiv = document.createElement('div');
-        wholeTextDiv.classList.add('text-box');
-        contentDiv.style.display = 'block';
+          Object.keys(orderedObject).forEach((e) => {
+            const newFieldDiv = document.createElement('div');
+            newFieldDiv.classList.add('newFieldDiv');
+            const keySpan = document.createElement('span');
+            keySpan.classList.add('keySpan');
 
-        Object.keys(orderedObject).forEach((e) => {
-          const newFieldDiv = document.createElement('div');
-          newFieldDiv.classList.add('newFieldDiv');
-          const keySpan = document.createElement('span');
-          keySpan.classList.add('keySpan');
+            const valueSpan = document.createElement('span');
+            valueSpan.classList.add('valueSpan');
 
-          const valueSpan = document.createElement('span');
-          valueSpan.classList.add('valueSpan');
+            keySpan.innerText = e + ':';
+            valueSpan.innerText = orderedObject[e];
 
-          keySpan.innerText = e + ':';
-          valueSpan.innerText = orderedObject[e];
+            newFieldDiv.appendChild(keySpan);
+            newFieldDiv.appendChild(valueSpan);
+            wholeTextDiv.appendChild(newFieldDiv);
+          });
 
-          newFieldDiv.appendChild(keySpan);
-          newFieldDiv.appendChild(valueSpan);
-          wholeTextDiv.appendChild(newFieldDiv);
-        });
-
-        contentDiv.appendChild(wholeTextDiv);
+          contentDiv.appendChild(wholeTextDiv);
+        }
       });
 
       const sipInvite = document.getElementById('main-form__input').value;
